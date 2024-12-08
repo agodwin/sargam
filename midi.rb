b0b1a74532597bd3565a13462c9a56cc646d4b94
Tony Godwin <ajgodwin@gmail.com>
2024-12-08 14:25:36 +0100

    first commit

---
+++ midi.rb
@@ -0,0 +1,24 @@
+
+# LINUX
+#   bundle exec ruby midi.rb 
+#   Could not open library 'libasound.so': libasound.so: cannot open shared object file: No such file or directory
+
+
+
+
+
+require "micromidi"
+
+# prompt the user to select an input and output
+
+# @input = UniMIDI::Input.gets
+@output = UniMIDI::Output.gets
+
+MIDI.using(@output) do
+
+  5.times do |oct|
+    octave oct
+    %w{C E G B}.each { |n| play n, 0.5 }
+  end
+
+end
